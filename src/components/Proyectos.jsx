0b3b2edd1c123da0876e2bfebb2025293a462f60
Amado <amadodamis@gmail.com>
2023-02-13 18:36:36 -0300

    asd

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,5 +1,5 @@
 import "../css/proyectosStyle.css"
-import foto from "../img/asd.jpg"
+import Carrousel from "./Carrousel";
 
 
 function Proyectos(props) {
@@ -8,43 +8,24 @@ function Proyectos(props) {
 
     return (
         <div className="proyectos-style" >
-
+            <br></br>
             <h2 className="titulo">PROYECTOS COMO <span className="span-title-proyects">{profesion}</span> </h2>
+           
 
             <nav className="nav-proyects">
 
                 <ul className="ul-proyects">
                     <li className="li-proyects">  <a href="/" className="a-proyects">{proyecto1}</a> </li>
-                    <li className="li-proyects"> <a href="/"  className="a-proyects">{proyecto2}</a></li>
-                    <li className="li-proyects"><a href="/"  className="a-proyects">{proyecto3}</a></li>
-                    <li className="li-proyects"><a href="/"  className="a-proyects">{proyecto4}</a></li>
+                    <li className="li-proyects"> <a href="/" className="a-proyects">{proyecto2}</a></li>
+                    <li className="li-proyects"><a href="/" className="a-proyects">{proyecto3}</a></li>
+                    <li className="li-proyects"><a href="/" className="a-proyects">{proyecto4}</a></li>
                 </ul>
 
             </nav>
 
+            <Carrousel />
 
-
-            <article className="container-menu">
-
-
-                <div className="container-buttons-preview">
-
-                    <button className="button-left"><span className="span-left">Prev</span></button>
-
-                    <div className="container-preview">
-                        <img src={foto} alt="fotoCv" className="preview" />
-                    </div>
-
-                    <button className="button-right"><span className="span-right">Sig</span></button>
-
-
-                </div>
-
-
-            </article>
-            <br/>
-
-
+<br/>
         </div>
 
     );
