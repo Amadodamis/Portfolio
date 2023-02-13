f4309edeb8fd588a8dfa5160ad01907b03fa6251
Amado <amadodamis@gmail.com>
2023-02-13 15:41:41 -0300

    terminado del primer scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,46 @@ import React from "react";
 import "../css/HeaderStyle.css"
 
 function Header() {
+
+  let h2 = <h2 className="nombre">AMADO <span className="apellido">DAMIS</span></h2>
+  
+  let proyectos = <a href="/" >Proyectos</a>;
+  let gastronomia = <a href="/" >Gastronomia</a>
+  let curriculum = <a href="/" >Curriculum</a>
+  let linkedin = <a href="https://www.linkedin.com/in/amadodamis/" >Linkedin</a>
+  let github = <a href="https://github.com/Amadodamis" >Github</a>
+
+
   return (
     <nav className="nav-style">
-      <h2 className="nombre">AMADO <span className="apellido">DAMIS</span></h2>
 
-      <ul >
-        <li className="li-header-style">
-          <a href="/" className="a-header-style">Proyectos</a></li>
+      {h2}
 
-        <li className="li-header-style">
-          <a href="/" className="a-header-style">Gastronomia</a></li>
+      <ul className="menu-horizontal" >
 
-        <li className="li-header-style">
-          <a href="/" className="a-header-style">Curriculum</a></li>
+        <li > {proyectos} </li>
 
-        <li className="li-header-style">
-          <a href="https://www.linkedin.com/in/amadodamis/" className="a-header-style">Linkedin</a></li>
+        <li > {gastronomia} </li>
 
-        <li className="li-header-style">
-          <a href="https://github.com/Amadodamis" className="a-header-style">Github</a></li>
+        <li > {curriculum}
+
+          <ul className="menu-vertical">
+
+            <li ><a href="https://drive.google.com/file/d/14a5Duaq8BFCXHzjRf88C6tfEAyaGdIni/view?usp=sharing">Desarrollador</a></li>
+
+            <li ><a href="https://docs.google.com/document/d/1t1y5hQZPdFFbIUKGZY_lhobcZyhNfnzPUegwa1p4PC4/edit?usp=sharing" >Profesional gastronomico</a></li>
+
+            <li ><a href="https://drive.google.com/file/d/1XSxHOP81Fys0F373Svj722YCylvE1u_1/view?usp=sharing" >Developer</a></li>
+
+            <li ><a href="https://docs.google.com/document/d/1CfaCnM_4KZ-0vE4R7kBt7pLdLUPL-KXQc0zU0Oy-N6c/edit?usp=sharing">Gastronomic Professional</a></li>
+
+          </ul>
+
+        </li>
+
+        <li >{linkedin} </li>
+
+        <li > {github} </li>
       </ul>
     </nav>
   );
